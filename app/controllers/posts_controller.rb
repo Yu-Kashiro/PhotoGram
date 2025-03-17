39852f29e226d64fb18f22141bb4fe27e04460b3
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-03-17 16:06:12 +0900

    posts_controllerを作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,4 +5,23 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = current_user.posts.build(post_params)
+    if post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content, images: [])
+  end
+
 end
