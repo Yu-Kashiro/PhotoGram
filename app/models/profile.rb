ddffa34e95f30b4aed2d43b5db0ef784484001f1
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-03-21 13:10:05 +0900

    プロフィール写真の表示を修正

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,4 +2,12 @@ class Profile < ApplicationRecord
   belongs_to :user
   has_one_attached :avatar
 
+  def display_avatar
+    if avatar.attached?
+      avatar
+    else
+      'dummy_profile.png'
+    end
+  end
+
 end
