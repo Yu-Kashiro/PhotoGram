6cbebf457c0d85d9df4e8987a8442a2ba7dd3514
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-04-29 11:49:47 +0900

    デフォルト言語を英語に変更

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,7 @@ module PhotoGram
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    # デフォルトを日本語設定
-    config.i18n.default_locale = :ja
+    config.i18n.default_locale = :en
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
