2a41c8ff22d054ef7667d8a8e0a01c0fd461024c
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-05-12 11:31:32 +0900

    日本語対応追加

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,7 @@
 class ProfilesController < ApplicationController
-  before_action :authenticate_user!
-  before_action :set_profile
 
   def show
+    @profile = current_user.prepare_profile
     @post_count = current_user.posts.count
     @followers_count = current_user.followers.count
     @following_count = current_user.followings.count
@@ -10,22 +9,21 @@ class ProfilesController < ApplicationController
   end
 
   def edit
+    @profile = current_user.prepare_profile
   end
 
   def update
+    @profile = current_user.prepare_profile
     if params[:avatar].present?
-      current_user.profile.avatar.attach(params[:avatar])  # 🔹 profile に avatar を設定
+      current_user.profile.avatar.attach(params[:avatar])
       @profile.save
-      render json: { message: 'アップロード成功', url: url_for(current_user.profile.avatar) }, status: :ok
+      render json: { message: I18n.t('profiles.update.success'), url: url_for(current_user.profile.avatar) }, status: :ok
     else
-      render json: { error: 'ファイルがありません' }, status: :unprocessable_entity
+      render json: { error: I18n.t('errors.messages.file_not_found') }, status: :unprocessable_entity
     end
   end
 
   private
-  def set_profile
-    @profile = current_user.prepare_profile
-  end
 
   def profile_params
     params.require(:profile).permit(:display_name, :introduction, :avatar)
