2a4ab9fe452a61e481d3da5a1696a97bda855a5b
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-05-12 15:24:05 +0900

    mailerをリファクタリング

--- app/mailers/reply_mailer.rb
+++ app/mailers/reply_mailer.rb
@@ -1,5 +1,5 @@
 class ReplyMailer < ApplicationMailer
-  def new_reply_notification(comment, user)
+  def new_reply_notification(comment, replied_user)
     @comment = comment
     @replied_user = user
     mail(to: @replied_user.email, subject: "あなたに返信がありました")
